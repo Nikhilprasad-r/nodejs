0b807470cc16a9228a73d690e15414e3809153cc
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-10 16:22:42 +0530

    sending json

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
+const data = {
+  menu: {
+    id: "file",
+    value: "File",
+    popup: {
+      menuitem: [
+        { value: "New", onclick: "CreateNewDoc()" },
+        { value: "Open", onclick: "OpenDoc()" },
+        { value: "Close", onclick: "CloseDoc()" },
+      ],
+    },
+  },
+};
 app.get("/", (req, res) => {
   res.send("<button>hello</button>");
 });
-
+app.get("/data", (req, res) => {
+  res.json(data);
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
