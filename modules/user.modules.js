df6900004f24ec082d466693d2a6a18ccd734bf8
Nikhilprasad-r <nikhilprasadinbox@gmail.com>
2024-03-11 18:57:09 +0530

    monggose 1

---
+++ modules/user.modules.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
